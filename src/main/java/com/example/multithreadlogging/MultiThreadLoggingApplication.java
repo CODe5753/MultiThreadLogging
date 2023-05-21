67e2f286d1a07e1c5d7ffe75d58beaf3aef5f2b3
CODe1995 <code.57x53@gmail.com>
2023-05-21 15:24:59 +0900

    Initial commit

---
+++ src/main/java/com/example/multithreadlogging/MultiThreadLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreadlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadLoggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/multithreadlogging/MultiThreadLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multithreadlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
